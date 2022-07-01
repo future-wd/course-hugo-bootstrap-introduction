6c7923dd8783b54fbfa8645ebda0351e452ad7c9
Sean Emerson <emerson.sean.dev@gmail.com>
2022-07-01 00:59:45 -0700

    added conditional code

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,7 @@
-// create a dynamic year and insert into the js-year span
-document.getElementById('js-year').innerHTML = new Date().getFullYear();
+// target year div in footer
+const year = document.getElementById('js-year')
+// conditionally update the div with current year on runtime
+// to avoid error
+if (year) {
+  year.innerHTML = new Date().getFullYear();
+}
